f861430bc48452b294a2be8c5a9ca8ff58039c50
segerrity <ujy8jg@virginia.edu>
2024-12-05 15:49:44 -0500

    Update docusaurus.config.js

--- tsg-website/docusaurus.config.js
+++ tsg-website/docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
               },
               {
                 label: 'Meet The Team',
-                to: '/team'
+                to: '/team',
             ],
           },
         ],
